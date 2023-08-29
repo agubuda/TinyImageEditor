5fd6e37affbaa63ffa147a4cbc336d2589eefe88
Liu <liu.wangyang@outlook.com>
2023-08-29 11:12:10 +0800

    backup, working on directory

--- TinyImageEditor/src/texture.cpp
+++ TinyImageEditor/src/texture.cpp
@@ -1,6 +1,8 @@
 #include "GLFW/glfw3.h"
 #include "opencv.hpp"
 #include <opencv2/core.hpp>
+#include <filesystem>
+
 //#include <opencv2/imgcodecs.hpp>
 //#include <opencv2/highgui.hpp>
 #include "texture.h"
@@ -11,12 +13,12 @@
 
 
 
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb_image_write.h"
-#define STB_IMAGE_RESIZE_IMPLEMENTATION
-#include "stb_image_resize.h"
+//#define STB_IMAGE_IMPLEMENTATION
+//#include "stb_image.h"
+//#define STB_IMAGE_WRITE_IMPLEMENTATION
+//#include "stb_image_write.h"
+//#define STB_IMAGE_RESIZE_IMPLEMENTATION
+//#include "stb_image_resize.h"
 
 
 Texture::Texture(const std::string& path)
@@ -55,41 +57,41 @@ Texture::~Texture()
     glDeleteTextures(1, &m_textureID);
 }
 
-unsigned char* Texture::LoadSingleImage(const std::string& inPath) {
-    unsigned char* data;
-    int width, height, nrChannels;
-    data = stbi_load(inPath.c_str(), &width, &height, &nrChannels, 0);
-    return data;
-}
+//unsigned char* Texture::LoadSingleImage(const std::string& inPath) {
+//    unsigned char* data;
+//    int width, height, nrChannels;
+//    data = stbi_load(inPath.c_str(), &width, &height, &nrChannels, 0);
+//    return data;
+//}
 
 void Texture::ResizeImage(const std::string& inPath, const std::string& outPath, int outputChannel[])
 {
-    int width, height, nrChannels;
-    unsigned char* data = NULL;
-    data = stbi_load(inPath.c_str(), &width, &height, &nrChannels, 3);
+    //int width, height, nrChannels;
+    //unsigned char* data = NULL;
+    //data = stbi_load(inPath.c_str(), &width, &height, &nrChannels, 3);
 
-    unsigned int outputDataMem = outputChannel[0] *  outputChannel[1] * nrChannels;
-    unsigned char* outData = (unsigned char*)malloc(outputDataMem);
+    //unsigned int outputDataMem = outputChannel[0] *  outputChannel[1] * nrChannels;
+    //unsigned char* outData = (unsigned char*)malloc(outputDataMem);
 
-    //stbir_resize(data, width, height, nrChannels, outData, outputChannel[0], outputChannel[1], outputChannel[2], STBIR_TYPE_UINT8, nrChannels, STBIR_ALPHA_CHANNEL_NONE, 0,
-    //    STBIR_EDGE_CLAMP, STBIR_EDGE_CLAMP,
-    //    STBIR_FILTER_BOX, STBIR_FILTER_BOX,
-    //    STBIR_COLORSPACE_SRGB, nullptr
-    //            );
+    ////stbir_resize(data, width, height, nrChannels, outData, outputChannel[0], outputChannel[1], outputChannel[2], STBIR_TYPE_UINT8, nrChannels, STBIR_ALPHA_CHANNEL_NONE, 0,
+    ////    STBIR_EDGE_CLAMP, STBIR_EDGE_CLAMP,
+    ////    STBIR_FILTER_BOX, STBIR_FILTER_BOX,
+    ////    STBIR_COLORSPACE_SRGB, nullptr
+    ////            );
 
-    stbir_resize_uint8(data, width, height, 0,
-        outData, outputChannel[0], outputChannel[1], 0, nrChannels);
+    //stbir_resize_uint8(data, width, height, 0,
+    //    outData, outputChannel[0], outputChannel[1], 0, nrChannels);
 
-    //std::filesystem::create_directory(outPath);
-    //std::filesystem::
-    if ((outputChannel[2] > 0) && (outputChannel[2] < 5)) {
-        stbi_write_png(outPath.c_str(), outputChannel[0], outputChannel[1], nrChannels, outData, 0);
-        std::cout << "input channel Out of range." << std::endl;
+    ////std::filesystem::create_directory(outPath);
+    ////std::filesystem::
+    //if ((outputChannel[2] > 0) && (outputChannel[2] < 5)) {
+    //    stbi_write_png(outPath.c_str(), outputChannel[0], outputChannel[1], nrChannels, outData, 0);
+    //    std::cout << "input channel Out of range." << std::endl;
 
-    }
+    //}
 
-    stbi_image_free(data);
-    stbi_image_free(outData);
+    //stbi_image_free(data);
+    //stbi_image_free(outData);
 
     std::cout << outputChannel << std::endl;
 };
@@ -99,12 +101,52 @@ int Texture::a = 0;
 
 void Texture::OutputSingleChannalImage(const std::string& inPath, const std::string& outPath, int stride, int comp)
 {
-    int width, height, nrChannels;
-    unsigned char* data = NULL;
-    data = stbi_load(inPath.c_str(), &width, &height, &nrChannels, 4);
+    std::string outputDir = "D:/output";
 
-    unsigned int outputDataMem = 512 * 512 * 1;
-    unsigned char* outData = (unsigned char*)malloc(outputDataMem);
+    cv::Mat originalImg;
+    originalImg = cv::imread(inPath.c_str()); // 改成自己的图片路径
+    if (originalImg.empty())
+    {
+        std::cout << "请确认图像文件名是否正确" << std::endl;
+        /*return 0;*/
+    }
+    cv::Mat grayImg;
+    cv::cvtColor(originalImg, grayImg, cv::COLOR_BGR2GRAY);
+
+    cv::imshow("gray scale result", grayImg);
+
+    //outputDir + "grayScaleImage.png";
+
+    //creat directory and file
+    std::filesystem::path outputPath(outputDir);
+    try 
+    {
+        std::filesystem::create_directory(outputPath);
+        std::cout << "Successfully created directory " << outputPath << std::endl;
+    }
+    catch (std::filesystem::filesystem_error& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+
+
+    std::string fileName = std::filesystem::path(inPath).filename().string();
+    std::string fullOutputPath = outputDir + "/" + fileName;
+
+    cv::imwrite(fullOutputPath, grayImg);
+
+
+
+    std::cout << "succesfully convert file to "<< fullOutputPath << std::endl;
+
+    //cv::waitKey(0);
+
+    //int width, height, nrChannels;
+    //unsigned char* data = NULL;
+    ////data = stbi_load(inPath.c_str(), &width, &height, &nrChannels, 4);
+
+    //unsigned int outputDataMem = 512 * 512 * 1;
+    //unsigned char* outData = (unsigned char*)malloc(outputDataMem);
 
     //stbir_resize(data, width, height, nrChannels, outData, width, height, 1, STBIR_TYPE_UINT8, nrChannels, STBIR_ALPHA_CHANNEL_NONE, 0,
     //    STBIR_EDGE_CLAMP, STBIR_EDGE_CLAMP,
@@ -121,32 +163,23 @@ void Texture::OutputSingleChannalImage(const std::string& inPath, const std::str
 
 
 
-    if (comp < 0 || comp> 4) {
-        std::cout << "input channel Out of range." << std::endl;
-    }
-    else {
-        stbi_write_png(outPath.c_str(), width, height, comp, data, 0);
-        //stbi_write_jpg(outPath.c_str(), 512, 512, comp, data, 100);
+    //if (comp < 0 || comp> 4) {
+    //    std::cout << "input channel Out of range." << std::endl;
+    //}
+    //else {
+    //    //stbi_write_png(outPath.c_str(), width, height, comp, data, 0);
+    //    //stbi_write_jpg(outPath.c_str(), 512, 512, comp, data, 100);
 
-        Sleep(500);
-        std::cout << "output single Channel" << std::endl;
+    //    Sleep(500);
+    //    std::cout << "output single Channel" << std::endl;
 
-    }
+    //}
 
     //cv::Mat img(600, 800, CV_8UC3, cv::Scalar(100, 250, 30));
-    cv::Mat img;
-    img = cv::imread("D:/container_R2.png"); // 改成自己的图片路径
-    //img = cv::imread("D:/abc.bmp"); // 改成自己的图片路径
-    if (img.empty())
-    {
-        std::cout << "请确认图像文件名是否正确" << std::endl;
-        /*return 0;*/
-    }
-    cv::imshow("test", img);
-    cv::waitKey(0);
 
 
-    stbi_image_free(data);
+
+    //stbi_image_free(data);
     //stbi_image_free(outData);
 
 };
